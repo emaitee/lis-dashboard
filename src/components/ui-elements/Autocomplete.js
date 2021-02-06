d0a79897d32ccaab0de43d6e14108079309edbab
emaitee <issatoyin@gmail.com>
2021-02-06 23:20:30 +0100

    initial commit

---
+++ src/components/ui-elements/Autocomplete.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Typeahead } from "react-bootstrap-typeahead";
+import { FormGroup, Label } from "reactstrap";
+
+function Autocomplete(props) {
+  const { required, label, _ref } = props;
+  return (
+    <FormGroup>
+      {label && label !== "" ? (
+        <Label className="font-weight-bold">
+          {label}
+          {required && <span className="text-danger">*</span>}
+        </Label>
+      ) : null}
+      <Typeahead
+        id={`${props.label}-12`}
+        ref={_ref}
+        className={`border border-primary rounded ${props.className}`}
+        {...props}
+      />
+    </FormGroup>
+  );
+}
+
+export default Autocomplete;
---
+++ src/components/ui-elements/vertical-menu/ListMenuItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useLocation, Link } from "react-router-dom";
+import { primaryColor } from "variables";
+
+function ListMenuItem(props) {
+  const location = useLocation();
+  const active = location.pathname.includes(props.route);
+  return (
+    <Link
+      to={props.route}
+      className="list-group-item list-group-item-action py-2"
+      style={
+        active
+          ? { backgroundColor: "#eee", borderLeft: `3px solid ${primaryColor}` }
+          : null
+      }
+    >
+      {props.children}
+    </Link>
+  );
+}
+
+export default ListMenuItem;
