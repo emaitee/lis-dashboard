56d1386ac37e4842e64c95a84df11264c830720b
emaitee <issatoyin@gmail.com>
2021-02-07 00:44:25 +0100

    remove external components

--- src/components/ui-elements/CustomScrollbar.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function CustomScrollbar(props) {
-  const { children = [], height = "65vh" } = props;
-  return <div style={{ maxHeight: height, overflow: "auto" }}>{children}</div>;
-}
-
-export default CustomScrollbar;
